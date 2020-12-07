251cf6560ede731517f86330bf783773079d8ac3
Tovmas Zhamagortsyan <tovmaszhamagortsyan@Tovmass-MacBook-Pro.local>
2020-12-08 01:57:50 +0400

    tovmas

--- script.js
+++ script.js
@@ -1,49 +1,79 @@
 class Player{
-    health = 100
-    username = "Default User"
-    lives = 3
-    currentPosition = "Paris"
-
+    constructor(health,username,lives,currentPosition,initialdamage,weapon){
+    this.health=health;
+    this.username=username;
+    this.lives=lives;
+    this.currentPosition=currentPosition;
+    this.initialdamage=initialdamage
+    this.weapon=weapon
+    } 
     attack(target){
-        target.health -=10
+        target.health -=this.initialdamage + this.weapon
+
     }
     run(destination){
         this.currentPosition = destination
+
     }
+   
 }
 
 class Dragon{
-    health = 1000
-    name = "Ipsum"
-    
+    constructor(health,name,lives,currentPosition,initialdamage,weapon){
+        this.health=health
+        this.name=name
+        this.lives=lives
+        this.currentPosition=currentPosition
+        this.initialdamage=initialdamage
+        this.weapon=weapon
+    }
     attack(target){
-        target.health -=10
+        target.health -=this.initialdamage + this.weapon
     }
 }
 
-let player = new Player()
-let dragon = new Dragon()
+let weapons={
+    tank: 20,
+    smerch:50
+}
+
+let player = new Player(100,"Default User",3,"Paris",20,weapons.tank)
+let dragon = new Dragon(1000,"Ipsum",5,"Yerevan",50,weapons.smerch)
 
 
-let userHealthbar = document.querySelector(".user-healthbar")
-let dragonHealthbar = document.querySelector(".dragon-healthbar")
+let playerHealthbar= document.querySelector(".user-healthbar")
+let dragonHealthbar= document.querySelector(".dragon-healthbar")
 let playerName = document.querySelector(".player-name")
 let dragonName = document.querySelector(".dragon-name")
-let attackButton = document.querySelector(".attack")
-attackButton.addEventListener('click', function(){
-    player.attack(dragon)
+let dragonattackButton = document.querySelector(".dragonattack")
+
+
+dragonattackButton.addEventListener('click',function(){
+
+ player.attack(dragon)
 
 })
-playerName.innerHTML = player.username
-dragonName.innerHTML = dragon.name
+let playerattackButton = document.querySelector(".userattack")
+playerattackButton.addEventListener('click',function(){
 
+ dragon.attack(player)
+ 
 
+})
 setInterval(function(){
-    userHealthbar.innerHTML = `User Health: ${player.health}`
-    dragonHealthbar.innerHTML = `Dragon Health: ${dragon.health}`
+    playerHealthbar.innerHTML = `User Health: ${player.health}`
 
 },5000)
 
+
+setInterval(function(){
+ 
+    dragonHealthbar.innerHTML = `Dragon Health: ${dragon.health}`
+    
+},5000)
+
+playerName.innerHTML = player.username
+dragonName.innerHTML = dragon.name
 
 
 
